99565712be95b474b8454bafb22a646028415fd7
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-25 23:14:31 +0200

    add promise task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,39 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const form = evt.target;
+  const delay = Number(form.elements.delay.value);
+  const radioBtn = form.elements.state.value;
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (radioBtn === 'fulfilled') {
+        res(delay);
+      } else {
+        rej(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay} ms`,
+      });
+    })
+
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay} ms`,
+      });
+    });
+  form.reset();
+}
